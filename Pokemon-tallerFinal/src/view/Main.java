f0e0de800e023cfe7adc87679887398af72f3ca7
Valentina Zapata <valentinazapata@MacBook-Pro-de-Valentina-2.local>
2020-05-14 22:49:28 -0500

    codigo base

---
+++ Pokemon-tallerFinal/src/view/Main.java
@@ -0,0 +1,26 @@
+package view;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main("view.Main");
+	}
+
+	public void settings() {
+
+		size(800, 800);
+	}
+
+	public void setup() {
+
+	}
+
+	public void draw() {
+
+		background(0);
+	}
+
+}// cierra main
