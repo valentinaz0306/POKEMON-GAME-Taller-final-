238713a89d8a0bcf37ea52b3d30a8c8f8bf0053a
Wilson <wilsonst.suarez@hotmail.com>
2020-05-16 22:41:00 -0500

    Load a couple of image

--- Pokemon-tallerFinal/src/view/Main.java
+++ Pokemon-tallerFinal/src/view/Main.java
@@ -1,8 +1,21 @@
 package view;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Main extends PApplet {
+	
+	
+	//Image variables
+	PImage start;
+	PImage logo;
+	
+	
+	//Int variables
+	
+	int pantalla;
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -11,16 +24,34 @@ public class Main extends PApplet {
 
 	public void settings() {
 
-		size(800, 800);
+		size(1200, 700);
 	}
 
 	public void setup() {
+		//Load images
+		start = loadImage("image/start.png");
+		logo = loadImage("image/logo.png");
+		
+		
+		//Load int variables
+		pantalla = 0;
+		
 
 	}
 
 	public void draw() {
 
+		switch(pantalla) {
+		
+		case 0:
 		background(0);
+		image(start,0,0);
+		image(logo,0,0);
+		break;
+		
+		}
+		
+		
 		
 	}
 
